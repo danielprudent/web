c2dd70ea5e80963ac539f6fc7c1a87e0405752ec
danielprudent <52863416+danielprudent@users.noreply.github.com>
2020-02-04 20:23:28 -0500

    Update TP1_NumC2020.java

--- TP1_NumC2020.java
+++ TP1_NumC2020.java
@@ -1,11 +1,11 @@
 class Etudiant {
 	
 	private String date; // "29/06/1996"
-	private double cafe; // 2.0 cafe
+	private int cafe; // 2.0 cafe
                      
 
 
-public Etudiant(String date, double cafe)
+public Etudiant(String date, int cafe)
 {
 this.date  = date; // ce this est obligatoire
 
@@ -14,68 +14,68 @@ this.cafe  = cafe; // ce this est optionnel (facultatif)
 
 public Etudiant(String date)
 {
-this(date, 1.0); // note globale par défaut
+this(date, 1); // note globale par défaut
 }
 
-public int rangMois()
-{
-return Integer.parseInt( date.substring(3,6)) % 50 ;
-}
+//	public int rangMois()
+//	{
+//	return Integer.parseInt( date.substring(3,6)) % 50 ;
+//	}
 
-public String getMois()
-{
-String[] nomMois = { "Janvier", "Février", "Mars", "Avril", "Mai",
-                   "Juin", "Juillet", "Août", "Septembre",
-                   "Octobre", "Novembre", "Décembre"};
-return nomMois[rangMois()-1];
-
-}
-public int getdate()
-{
-return Integer.parseInt(date.substring(0,3));
-}
-
-
-
-public int getJour()
-{
-return Integer.parseInt(date.substring(0,3));
-}
-
-public int getAnnee()
-{
-return  Integer.parseInt(date.substring(7));
-}
-
-public String getSexe()
-{
-return date.charAt(6) >= '5' ? "féminin":"masculin";
-}
+//	public String getMois()
+//	{
+//	String[] nomMois = { "Janvier", "Février", "Mars", "Avril", "Mai",
+//	                   "Juin", "Juillet", "Août", "Septembre",
+//	                   "Octobre", "Novembre", "Décembre"};
+//	return nomMois[rangMois()-1];
+//	
+//	}
+//	public int getdate()
+//	{
+//	return Integer.parseInt(date.substring(0,3));
+//	}
 
 
-public void afficher()
-{
-System.out.printf("%s date: %s  cafe : %d %s %d globale : %.1f\n",
-	date, getSexe(), getJour(), getMois(), getAnnee(), cafe);
-}
 
-public String getDT()
-{
-return date;
-}
+//	public int getJour()
+//	{
+//	return Integer.parseInt(date.substring(0,3));
+//	}
 
-public double getCF()
-{
-return cafe;
-}
-public void setGlobale(double nouvGlobale)
-{ cafe = nouvGlobale;
-if (cafe > 100.0)
-	   cafe = 100.0;
-if (cafe < 0.0)
-	    cafe = 0.0;
+//	public int getAnnee()
+//	{
+//	return  Integer.parseInt(date.substring(7));
+//	}
 
-}
+	//public String getSexe()
+	//{
+	//return date.charAt(6) >= '5' ? "féminin":"masculin";
+	//}
+
+
+	public void afficher()
+	{
+	System.out.printf("%s date: %s  cafe : %d %s %d globale : %.1f\n",
+		date, cafe);
+	}
+
+	public String getDT()
+	{
+	return date;
+	}
+
+	public int getCF()
+	{
+	return cafe;
+	}
+	public void setGlobale(int nouvGlobale)
+	{ cafe = nouvGlobale;
+	if (cafe > 100)
+		   cafe = 100;
+	if (cafe < 0)
+		    cafe = 0;
+	
+	}
 
 	
 	
@@ -85,22 +85,51 @@ public class TestPersonne {
 	
 	 static void demo2() {
     System.out.printf("\nIndice	Date	Café :\n");
-    Etudiant[] etud = { new Etudiant("	16/05/1992", 2.0),
+    Etudiant[] etud = { new Etudiant("	16/05/1992", 2),
                         new Etudiant("	02/01/1990"), // 1 tasse par defaut
-                        new Etudiant("	23/05/1990", 5.0),
-                        new Etudiant("	19/02/1985", 0.0),
-                        new Etudiant("	30/05/1991", 2.0),
-                        new Etudiant("	31/01/1990", 4.0)};
+                        new Etudiant("	23/05/1990", 5),
+                        new Etudiant("	19/02/1985", 0),
+                        new Etudiant("	30/05/1991", 2),
+                        new Etudiant("	31/01/1990", 4)};
     int nbEtud = etud.length;
+    
 	 
-   
+
+//    static void trier(Etudiant[] etud, int nbEtud)
+//    {
+//    	for(int i = 0; i < nbEtud-1; i++)
+//    	{
+//    		int indMin = i ;
+//    		for(int j = i+1; j < nbEtud; j++)
+//    			if (etud[j].getCF() < etud[indMin].getCF())
+//    				         indMin = j;
+//    	    if (indMin != i)
+//    	    {
+//    	    	Etudiant tempo = etud[i];
+//    	    	etud[i] = etud[indMin];
+//    	    	etud[indMin] = tempo;
+//    	    }
+//    	}
+//    }
      afficher (etud, "au début");
     // System.out.printf("Le nombre d'étudiants nés au mois de septembre : %d\n",
      //         nombre(etud, nbEtud,9))  ;
      //trier(etud, nbEtud); // selon les notes globales
 
      afficher(etud, "après le tri selon les notes globales");
-     System.out.printf("\nLe min .....: %f", min(etud, nbEtud));
+     //System.out.printf("\nLe min de cafe consommer: %.1f", min(etud, nbEtud));
+     
+
+     
+     //etud[min(etud, nbEtud)].
+    int m = min(etud, nbEtud);
+    
+
+	
+
+	System.out.println(m + ")" + "\t" + etud[m].getDT() + "\t" + etud[m].getCF() + " tasses");
+     
+
 }
 	 
 	 
@@ -112,31 +141,25 @@ static void afficher (Etudiant[] etud, String mess) {
 	}    		   
     System.out.printf("\n\n");
 }
-static double min(Etudiant[]  etud, int nbP)    { // condition pour le cafe plus grand et les infos de la personne
-	double pluspetit = 1000000;
+static int min(Etudiant[]  etud, int nbP)    { // condition pour le cafe min et les infos de la personne
+	int plusPetit = 5;
+	for(int i = 0; i < nbP; i++)
+   	 	if (etud[i].getCF() < etud[plusPetit].getCF())
+   	 	    plusPetit = i;
+
+   return plusPetit;
+   
+}
+
+static int minOLD(Etudiant[]  etud, int nbP)    { // condition pour le cafe plus grand et les infos de la personne
+	int pluspetit = 1000000;
 	for(int i = 0; i < nbP; i++)
    	 	if (etud[i].getCF() < pluspetit)
    	 		pluspetit = etud[i].getCF();
 
    return pluspetit;
-}
 
-//	    static void trier(Etudiant[] etud, int nbEtud)
-//	    {
-//	    	for(int i = 0; i < nbEtud-1; i++)
-//	    	{
-//	    		int indMin = i ;
-//	    		for(int j = i+1; j < nbEtud; j++)
-// 			if (etud[j].getGlobale() < etud[indMin].getGlobale())
-//	    				         indMin = j;
-//	    	    if (indMin != i)
-//	    	    {
-//	    	    	Etudiant tempo = etud[i];
-//	    	    	etud[i] = etud[indMin];
-//	    	    	etud[indMin] = tempo;
-//	    	    }
-//	    	}
-//    }
+}
 
 
 	 
@@ -144,7 +167,6 @@ static double min(Etudiant[]  etud, int nbP)    { // condition pour le cafe plus
 	public static void main(String[] args) {
 		
 		demo2();
-		//System.out.printf("\nLe min .....: %f", min(etud, nbEtud));
 
 		System.out.printf("\n\n");      
 		
