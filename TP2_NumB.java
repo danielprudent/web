6d7349d7ab2dc88d6a9b530d17a95c6d0eb9b76b
danielprudent <52863416+danielprudent@users.noreply.github.com>
2020-03-10 22:27:19 -0400

    Update TP2_NumB.java

--- TP2_NumB.java
+++ TP2_NumB.java
@@ -3,6 +3,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import org.w3c.dom.ranges.RangeException;
+
 class Pays {
 	
 	//1- Déclaration des Attributs :
@@ -48,7 +50,7 @@ class Pays {
 	}
 //}
 	public String toString() {  
-		return String.format("2 & 3) %-30c %8s %7s %7.0f %7.0f", continent, pays, capital, population, superficie);
+		return String.format("%-30c %8s %7s %7.0f %7.0f", continent, pays, capital, population, superficie);
 	}
 	
 	public boolean equals(Object obj) {
@@ -90,7 +92,7 @@ public class Application {
 		if (s.equals("")) System.out.printf("\nAucun pays n'a le nom de sa capitale !!!");
 		else	
 			System.out.printf("\n4- Les pays dont le nom égal au nom de sa capitale sont :\n%s", s);
-		
+		System.out.printf("\n\n");
 	}
 	
 	static int lireRemplir(String nomFichier) throws IOException {
@@ -170,9 +172,84 @@ public class Application {
 			  System.out.println(pays[i].toString());
 		  }
 	  }
+		static int partitionner ( Pays[]T, int debut, int fin)
+		{ int g = debut , d = fin  ;
+	      String valPivot  = T[debut].getPays();
+	      Pays tempo ;
 
+	  	  do
+		  {   while ( g <= d && (T[g].getPays().compareTo(valPivot)<=0)) g++;
+		      while ( T[d].getPays().compareTo(valPivot)> 0) d--;
 
-public static void main(String[] args) {
+			  if ( g < d )
+			  	// echanger(T, g, d);
+
+			  { tempo = T[g];
+				T[g] = T[d];
+				T[d] = tempo;
+	          }
+	 }	while ( g <= d ) ;
+		  // echanger(T, debut, d);
+
+		  tempo = T[debut];
+		  T[debut] = T[d];
+		  T[d] = tempo;
+
+		  return d;
+		}
+	  
+	  static void quickSort (Pays [] T, int gauche, int droite)
+	  {  if (droite > gauche) /* au moins 2 ?l?ments */
+			{
+			  int indPivot = partitionner ( T, gauche, droite);
+			  quickSort ( T, gauche, indPivot - 1 );
+			  quickSort ( T, indPivot + 1, droite);
+			}
+	  }
+	  static int indDicho(String aChercher, int nbElem, Pays[] t)
+	   {
+	    int mini = 0, maxi = nbElem - 1;
+
+	    while (mini <= maxi) {
+	               int milieu = (mini + maxi) / 2 ;
+	               if ( aChercher.trim().compareToIgnoreCase(t[milieu].getPays().trim()) < 0)
+	                    maxi = milieu - 1 ;
+	               else  if (aChercher.trim().compareToIgnoreCase(t[milieu].getPays().trim()) > 0)
+	                             mini = milieu + 1 ;
+	                     else return milieu;
+	     }
+	    return -1 ; // non trouvé
+	   }
+		static void creerTexte(char paysVoulu, Pays[] pays, int nbPers, String nomACreer)
+				throws IOException
+			{
+
+				boolean probleme = false;
+				FileWriter fw = null;
+				try
+				{
+				    fw = new FileWriter(nomACreer);
+				} catch (java.io.FileNotFoundException erreur)
+				{
+					System.out.println("Problème de préparer l'écriture\n");
+					probleme = true;
+				}
+				if (!probleme)
+				{
+					System.out.println("Début de la création du fichier\n");
+					PrintWriter aCreer = new PrintWriter( fw );
+
+					for (int i = 0; i < nbPers; i++)
+						if (pays[i].getPays().equals(paysVoulu))
+							aCreer.printf("%s\n", pays[i]);
+
+					aCreer.close();
+					System.out.println("Fin de la création du fichier " +
+									      nomACreer + "\n\n");
+				}
+
+			}
+public static void main(String[] args) throws IOException {
 	
 	int MAX_NB_PAYS=250;
 	pays = new Pays[MAX_NB_PAYS];
@@ -193,6 +270,40 @@ public static void main(String[] args) {
 	
 	//4. Afficher les pays dont le nom est identique au nom de la capitale (exemple	Luxembourg)
 	nomPaysEgalNomCap(pays, nbPays);
+	
 	//5. Trier (rapide) selon les noms des pays et d’afficher les 10 premiers pays après le tri
+	 quickSort(pays, 0, nbPays-1);
+     afficherDixPremier();//(pays, nbPays, "apres le tri");
+     creerTexte('5', pays, nbPays, "Europe.txt");
+ 	 creerTexte('3', pays, nbPays, "Asie.txt");
+     String[] aChercher = { "Canada", "France", "Japon", "Mexique" };
+     int nbPersRecherchee = aChercher.length;
+
+     for(int i = 0; i < nbPersRecherchee ; i++)
+     {
+     	 System.out.printf("La recherche dichotomique de %s\n", aChercher[i]);
+     	 int k = indDicho(aChercher[i], nbPays, pays);
+     	 if ( k < 0)
+     	 	System.out.printf("On ne trouve pas cette personne\n\n");
+     	 else
+     	 {
+     	 	// à expliquer en classe
+     	 	int debut = k, fin = k ;
+     	 	while(debut >= 0 && pays[debut].equals(pays[k])) debut--;
+     	 	debut++;
+
+     	 	while(fin < nbPays && pays[fin].equals(pays[k])) fin++;
+     	 	fin--;
+
+     	 	if (debut == fin)
+                  System.out.printf("L'unique personne trouvee :\n%s\n", pays[k]);
+           else
+           	   for(int z = debut; z <= fin ; z++)
+           	   	  System.out.printf("%5d) %s\n", z, pays[z]);
+     	 	
+     	 }
+     }
+  
+	
 }//main
 }
